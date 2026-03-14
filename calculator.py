c6341cadbd509a92fe7e136eaa1996b886147384
artemwoka <artemwoka@gmail.com>
2026-03-14 14:49:42 +0100

    Add reset method to CalculatorApp and call it during initialization

--- calculator.py
+++ calculator.py
@@ -31,6 +31,7 @@ class ExtraActionButton(CalcButton):
 class CalculatorApp(ft.Container):
 
     def init(self):
+        self.reset()
         self.width = 350
         self.bgcolor = ft.Colors.BLACK
         self.border_radius = ft.BorderRadius.all(20)
@@ -91,6 +92,11 @@ class CalculatorApp(ft.Container):
                 self.result.value = "1"
             else:
                 self.result.value += "1"
+    
+    def reset(self):
+        self.operator = '+'
+        self.operand1 = 0
+        self.new_operand = True
         
 
             
