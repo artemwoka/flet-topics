f1787e36df9f678e3c61ed51f429ad6b91b7fcf2
artemwoka <artemwoka@gmail.com>
2026-03-14 13:23:19 +0100

    Updated version

--- calculator.py
+++ calculator.py
@@ -1,13 +1,15 @@
-from dataclasses import field
-
 import flet as ft
 
 
 @ft.control
 class CalcButton(ft.Button):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        expand = 1
+    pass
+    # expand: int = 1
+
+    # def __init__(self, *args, **kwargs):
+    # super().__init__(*args, **kwargs)
+    # self.expand = 1
+    # self.expand_loose = False
 
 
 @ft.control
@@ -19,60 +21,89 @@ class DigitButton(CalcButton):
 @ft.control
 class ActionButton(CalcButton):
     bgcolor: ft.Colors = ft.Colors.ORANGE
-    color: ft.Colors.WHITE
+    color: ft.Colors = ft.Colors.WHITE
 
 
 @ft.control
 class ExtraActionButton(CalcButton):
     bgcolor: ft.Colors = ft.Colors.BLUE_GREY_100
-    color: ft.Colors.BLACK
+    color: ft.Colors = ft.Colors.BLACK
+
+
+@ft.control
+class CalculatorApp(ft.Container):
+
+    def init(self):
+        self.width = 290
+        self.bgcolor = ft.Colors.BLACK
+        self.border_radius = ft.BorderRadius.all(20)
+        self.padding = 20
+        self.result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
+
+        self.content = ft.Column(
+            controls=[
+                ft.Row(controls=[self.result], alignment=ft.MainAxisAlignment.END),
+                ft.Row(
+                    controls=[
+                        ExtraActionButton("AC", on_click=self.button_clicked),
+                        ExtraActionButton("+-"),
+                        ExtraActionButton("%"),
+                        ActionButton("/"),
+                    ],
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton("7"),
+                        DigitButton("8"),
+                        DigitButton("9"),
+                        ActionButton("*"),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton("4"),
+                        DigitButton("5"),
+                        DigitButton("6"),
+                        ActionButton("-"),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton("1", on_click=self.button_clicked),
+                        DigitButton("2"),
+                        DigitButton("3"),
+                        ActionButton("+"),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton("0", expand=2),
+                        DigitButton("."),
+                        ActionButton("="),
+                    ]
+                ),
+            ]
+        )
+
+    def button_clicked(self, event):
+        data = event.control.content
+        if data == "AC":
+            self.result.value = "0"
+        elif data == "1":
+            if self.result.value == "0":
+                self.result.value = "1"
+            else:
+                self.result.value += "1"
+        
+
+            
+
 
 def main(page: ft.Page):
-    page.title = "Calculator"
-    result = ft.Text(value = '0', color = ft.Colors.WHITE, size = 20)
+    page.title = "Кальк улятор"
+    calc = CalculatorApp()
+    page.add(calc)
 
-    page.add(
-        ft.Container(
-            width = 350,
-            bgcolor= ft.Colors.BLACK,
-            border_radius = ft.BorderRadius.all(20),
-            padding= 20,
-            content =ft.Column(
-                controls = [
-        ft.Row(controls = [result]),
-        ft.Row(controls = [
-            ExtraActionButton("AC"),
-            ExtraActionButton("+/-"),
-            ExtraActionButton("%"),
-            ActionButton("/"),
-        ]),
-        ft.Row(controls = [
-            DigitButton("7"),
-            DigitButton("8"),
-            DigitButton("9"),
-            ActionButton("*")
-        ]),
-        ft.Row(controls = [
-            DigitButton("4"),
-            DigitButton("5"),
-            DigitButton("6"),
-            ActionButton("-")
-        ]),
-        ft.Row(controls = [
-            DigitButton("1"),
-            DigitButton("2"),
-            DigitButton("3"),
-            ActionButton("+")
-        ]),
-        ft.Row(controls = [
-            DigitButton("0", expand = 2),
-            DigitButton("."),
-            ActionButton("=")
-        ])
-        ]
 
-        
-        
-            )))
 if __name__ == "__main__":
     ft.run(main)
