4aafb326aaea688f05d3f49813063248fc3c3349
artemwoka <artemwoka@gmail.com>
2026-03-28 15:43:47 +0100

    Refactor Todo app to use class-based structure for improved organization and maintainability

--- todo.py
+++ todo.py
@@ -1,30 +1,44 @@
 import flet as ft
 
 
-def main(page: ft.Page):
-    def add_clicked(e):
-        page.add(ft.Checkbox(label=new_task.value))
-        new_task.value = ""
-        page.update()
-    
-    new_task = ft.TextField(hint_text= "Що потрібно зробити?", expand = True)
-    tasks_view = ft.Column()
-    cointainer = ft.Column(
-        width = 600,
-        controls = [
-            ft.Row(
-                controls = [
-                    new_task,
-                    ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked)
-                ]
-            ),
-            tasks_view,
-        ],
-    )
 
+@ft.control
+class TodoApp(ft.Column):
+
+    def init(self):
+        self.new_task = ft.TextField(hint_text= "Що потрібно зробити?", expand = True)
+        self.tasks_view = ft.Column()
+        self.width = 600
+        self.controls = ft.Column(   
+            controls = [
+                ft.Row(
+                    controls = [
+                        self.new_task,
+                        ft.FloatingActionButton(
+                            icon=ft.Icons.ADD, on_click=self.add_clicked
+                            )
+                    ],
+                ),
+                self.tasks_view,
+            ],
+        )
+
+
+    def add_clicked(self, e):
+        self.tasks_view.controls.append(ft.Checkbox(label=self.new_task.value))
+        self.new_task.value = ""
+        self.update()
+
+
+def main(page: ft.Page):
+    
+        
+    page.title = "Todo App"
     page.horizontal_aligment = ft.CrossAxisAlignment.CENTER
-    page.add(cointainer)
     page.window.width = 600
     page.update()
+
+    todo = TodoApp()
+    page.add(todo)
 
 ft.run(main)
